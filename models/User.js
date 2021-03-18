1ed6022a71130f45eb9a02c09d7bb8078c59671b
Cory Calaway <cory.c.calaway@gmail.com>
2021-03-18 16:42:57 -0600

    starting to set up models

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+const { DateTime } = require('luxon');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'You must provide a username!',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'You must provide a valid email address!',
+        unique: true,
+        // Must match a valid email address (look into Mongoose's matching validation)
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+
+})
+
+const User = model ('User', UserSchema);
+
+module.exports = User;
