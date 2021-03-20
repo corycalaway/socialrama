a77503ab1bc36248ed21bc95a8e7aaf72d9f51b7
Cory Calaway <cory.c.calaway@gmail.com>
2021-03-19 20:54:13 -0600

    added get all thoughts route

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -34,6 +34,16 @@ thoughtController = {
           })
           .catch(err => res.json(err));
       },
+
+   
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 }
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
 const {
-    createThought
+    createThought,
+    getAllThoughts
 } = require('../../controllers/thought-controller')
 
 router
     // thoughs
     .route('/')
     .post(createThought)
+    .get(getAllThoughts)
 
 module.exports = router;
